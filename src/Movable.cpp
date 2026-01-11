2f0ef03d6e4a234a773b1f49b9de00f4ad1b98e4
Elosys5 <blazewiczbartosz783@gmail.com>
2026-01-11 16:46:55 +0100

    refactor: clean up Movable as pure movement base class

--- src/Movable.cpp
+++ src/Movable.cpp
@@ -6,25 +6,57 @@ Movable::Movable(
     sf::Vector2f size
 )
     : GameObj(position, size),
-      speed_vector(0.f, 0.f),
-      max_speed(0.f),
-      min_speed(0.f)
-{}
+    speed_vector(0.f, 0.f),
+    max_speed(0.f),
+    min_speed(0.f)
+{
+}
 
-sf::Vector2f Movable::getSpeedVector(){
+sf::Vector2f Movable::getSpeedVector() const {
     return speed_vector;
 }
 
-void Movable::bounceX(){
+void Movable::setSpeed(const sf::Vector2f& speed) {
+    speed_vector = speed;
+    limitSpeed();
+}
+
+void Movable::addSpeed(const sf::Vector2f& deltaSpeed) {
+    speed_vector += deltaSpeed;
+    limitSpeed();
+}
+
+void Movable::limitSpeed() {
+    float length = std::sqrt(
+        speed_vector.x * speed_vector.x +
+        speed_vector.y * speed_vector.y
+    );
+
+    if (max_speed > 0.f && length > max_speed) {
+        speed_vector *= (max_speed / length);
+    }
+}
+
+void Movable::updateMovement(float delta) {
+    move(sf::Vector2f(
+        speed_vector.x * delta,
+        speed_vector.y * delta
+    ));
+}
+
+void Movable::bounceX() {
     speed_vector.x *= -0.5;
 }
 
-void Movable::bounceY(){
+void Movable::bounceY() {
     speed_vector.y *= -0.5;
 }
 
 void Movable::move(sf::Vector2f delta) {
-    setPosition(sf::Vector2f(getPosition().x + delta.x, getPosition().y + delta.y));
+    setPosition(sf::Vector2f(
+        getPosition().x + delta.x,
+        getPosition().y + delta.y
+    ));
     /*
     if (
         x < 0.f && getPosition().x + x > 0.f ||
@@ -42,12 +74,3 @@ void Movable::move(sf::Vector2f delta) {
         )
         setPosition(sf::Vector2f(getPosition().x, getPosition().y + y));
     else {
-        if (std::abs(speed_vector.y) > min_speed)
-            speed_vector.y *= -0.8;
-    }
-    */
-}
-
-void Movable::stop() {
-    speed_vector = { 0.f, 0.f };
-}
